fe64f7cc3cf5d038b501a940e77b57aa26fee46a
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-06-09 19:24:17 +0300

    form with password added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+
+
   def index
     @users = User.order(params[:sorting].nil? ? "id" : params[:sorting]).paginate(per_page: 5, page: params[:page]) 
   end
@@ -21,7 +22,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update_attributes(name: params[:user][:name], lastname: params[:user][:lastname], email: params[:user][:email])
+    @user.update_attributes(user_params)
     if @user.errors.empty?
       redirect_to @user
     else
@@ -30,8 +31,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(name: params[:user][:name], lastname: params[:user][:lastname], email: params[:user][:email])
+    @user = User.create(user_params)
     if @user.errors.empty?
+      #flash[:success] = "Welcome to LoanApp!"
       redirect_to user_path(@user)
     else
       render "new"
@@ -43,5 +45,10 @@ class UsersController < ApplicationController
     @user.destroy
     redirect_to users_path
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :lastname, :password, :password_confirmation)
+    end
 
 end
