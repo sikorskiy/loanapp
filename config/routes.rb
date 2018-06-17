bd21602b2f4fed7ca0dac065e1dc062096d15c34
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-06-17 15:49:56 +0300

    login and logout added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'users#home'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
   resources :users
   resources :loans
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
